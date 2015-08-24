aa70b893ea7f927c697ab7bcde160b8ec8dea809
Lee Cooper <lee.cooper@lski.uk>
2015-08-24 17:14:14 +0100

    Slight update the nuget package

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: ComVisible(false)]
 [assembly: Guid("af8c8c52-45aa-4dfa-a59e-2091171b1eb0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.*")]
